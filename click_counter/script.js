86391637f2490b70473782bc0f85841e9a5d1ae6
Cory545 <Coryburton545@gmail.com>
2025-07-25 20:31:30 +0100

    Finish project

--- click_counter/script.js
+++ click_counter/script.js
@@ -1,10 +1,38 @@
 let counterNumber = 0;
 let button = document.getElementById("counterButton");
+let resetButton = document.getElementById("resetButton");
+let minusButton = document.getElementById("minusButton");
+let addFiveButton = document.getElementById("addFiveButton");
+let minusFiveButton = document.getElementById("minusFiveButton");
 let counter = document.getElementById("counter");
 counter.innerText = counterNumber;
 
 button.addEventListener("click", counterFunction);
 function counterFunction(){
     counterNumber++;
+    counter.innerText = counterNumber;
+};
+
+resetButton.addEventListener("click", resetFunction);
+function resetFunction(){
+    counterNumber = 0;
+    counter.innerText = counterNumber;
+};
+
+minusButton.addEventListener("click", minusFunction);
+function minusFunction(){
+    counterNumber--;
+    counter.innerText = counterNumber;
+};
+
+addFiveButton.addEventListener("click", addFiveFunction);
+function addFiveFunction(){
+    counterNumber += 5;
+    counter.innerText = counterNumber;
+};
+
+minusFiveButton.addEventListener("click", minusFiveFunction);
+function minusFiveFunction(){
+    counterNumber -= 5;
     counter.innerText = counterNumber;
 };
